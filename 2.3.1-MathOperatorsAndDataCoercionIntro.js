b04f86b8ebd38fac5ae39c456e30d2f74ffe887c
Mboy3000 <31638429+Mboy3000@users.noreply.github.com>
2023-06-21 18:48:36 -0500

    Add files via upload
    Work we did going over  operators and flow control.

---
+++ 2.3.1-MathOperatorsAndDataCoercionIntro.js
@@ -0,0 +1,24 @@
+// What should we expect to print?
+
+// Basic Math
+console.log(1 + 2 * 3);
+// output 7
+console.log((1 + 2) * 3);
+//output 9
+console.log(40 / 5 - 2 ** 3 );
+// output 0
+console.log(6 % 2);
+//output 0
+
+// Adding/Concatenating Strings
+console.log("10" + "5");
+//output 105
+console.log("Hello " + "world");
+//output 105
+
+// Data Coercion
+console.log("2" + 2);
+console.log(2 + true);
+console.log(true + true + "true" + "false");
+console.log(1 + 1 + "2" + "3");
+
