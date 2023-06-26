db095ea75f293d8929435fe32e80c2b3836ca349
Maxwell Heenan <Maxwell.heenan@gmail.com>
2023-06-26 08:25:10 -0500

    test

---
+++ 2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,31 @@
+// // create a constant variable (const)
+const anyCode = "somthing";
+// // create a variable that can be reassigned (let)
+let tacos= 23;
+// // create three variables and assign them values of different data types (=)
+ let name="Maxwell";
+ let money = 42;
+let enough=true;
+// print the types of two variables that reference two different data types (typeof)
+ console.log(typeof(name));
+ console.log(typeof(money));
+
+// TIP: console.log() prints a value in the console/terminal
+
+// change the value referenced by a variable (dynamic typing)
+console.log(money+"10");
+console.log("10"+ money);
+console.log(name+money);
+
+// print the type of the variable you just changed (typeof)
+console.log(typeof(money));
+
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+let language="Javascript";
+let number= 2;
+let masterStatus= false;
+
+ console.log("Hello, my name is " + name + ", and I am learning " + language + ". I have been practicing for only " +  number + " weeks, so my status as a master is: " + masterStatus + ".");
+
