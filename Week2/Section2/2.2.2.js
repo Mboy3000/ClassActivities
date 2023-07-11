b4313a3b7bb5c823004e1170c34f9d4b24086e37
Maxwell Heenan <Maxwell.heenan@gmail.com>
2023-07-11 09:14:17 -0500

    2.2.2

---
+++ Week2/Section2/2.2.2.js
@@ -0,0 +1,38 @@
+// create a variable called "name" that references a string
+
+// create a variable called "favoriteSong" that references a string
+let favoriteSong ="I wanna stay at your house"
+// create a variable called "wage" that references a number
+let wage = 20;
+// create a variable called "age" that references a number
+let age = 26;
+// create a variable called "onlyChild" that references a boolean
+let onlyChild=false;
+// create a variable called "satisfied" that references a boolean
+let satisfied = true;
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+let favoriteThing = "video games!"
+console.log(favoriteThing);
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong ="stary heavens"
+
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteSong= 42;
+console.log(typeof(favoriteSong));
+
+// Happy Birthday!
+// Reassign "age" to a new value
+age=27;
+
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage= 32;
+// Your raise chan2.2ged your job satisfaction.
+// Assign "satisfied" to true
+
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+
+console.log("hello my name is Maxwell I am currently" + age+ "years old." +" my favorite song is" + favoriteSong +"by the day after tomorrow .")
