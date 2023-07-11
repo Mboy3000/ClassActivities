682f850c365a7b3d747744927bd5461851dba05f
Maxwell Heenan <Maxwell.heenan@gmail.com>
2023-07-11 08:28:30 -0500

    stuff

---
+++ Week2/Section2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,29 @@
+// create a constant variable (const)
+const variable = 32;
+
+// create a variable that can be reassigned (let)
+let pizzaVariable =20;
+// create three variables and assign them values of different data types (=)
+let VariableOne;
+ VariableOne = "string variable";
+let VariableTwo = 20;
+let VariableThree= true;
+// print the types of two variables that reference two different data types (typeof)
+console.log(typeof(VariableOne));
+console.log(typeof(VariableTwo));
+// TIP: console.log() prints a value in the console/terminal
+
+// change the value referenced by a variable (dynamic typing)
+  VariableOne= 40;
+// print the type of the variable you just changed (typeof)
+console.log(typeof(VariableOne));
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// name , language, number, a true or false statement
+// What type of data needs to go in each variable?
+// 2 strings, a number and a bool 
+let name= "maxwell";
+let lang= "java script";
+let numWeeks = 3;
+let Master =false;
+console.log(`Hello, my name is ${name}, and I am learning ${lang}. I have been practicing for only ${numWeeks} weeks, so my status as a master is: ${Master}` ,".");
