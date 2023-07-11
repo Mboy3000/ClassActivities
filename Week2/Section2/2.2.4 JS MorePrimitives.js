a108a238abcab61b60482d4c433a4412116afcb3
Maxwell Heenan <Maxwell.heenan@gmail.com>
2023-07-11 09:32:03 -0500

    2.2.4 JS MorePrimitives

---
+++ Week2/Section2/2.2.4 JS MorePrimitives.js
@@ -0,0 +1,22 @@
+// create five variables and assign them values
+let VarOne ="string"
+let VarTwo=true;
+let VarThree= 10;
+let VarFour=undefined;
+let VarFive=null;
+// each variable should reference a different primitive data type
+// REMEMBER: you should never DELIBERATELY assign "undefined" to a variable
+
+// print the type of each variable in this order:
+// string, number, boolean, undefined, null
+console.log(typeof(VarOne)+ typeof(VarThree)+ typeof(VarTwo)+typeof())
+// create a variable that references a template literal
+// inside the template literal, use two of the above variables
+
+// reassign the value of the variable that references "null"
+
+// print the value and its type
+
+// print a variable that causes a ReferenceError
+
+// alter the previous line to no longer cause a ReferenceError
