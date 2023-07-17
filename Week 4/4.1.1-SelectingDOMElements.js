544dfadb531ac78f77d19e0eadfe9bdd1e907ba4
Maxwell Heenan <Maxwell.heenan@gmail.com>
2023-07-17 16:48:55 -0500

    class activities

---
+++ Week 4/4.1.1-SelectingDOMElements.js
@@ -0,0 +1,11 @@
+// Select all unordered list (ul) elements
+const ulElements = document.querySelectorAll('ul');
+
+// Select all elements with the class "class-week"
+const classWeekElements = document.querySelectorAll('.class-week');
+
+// Select all elements with the class "nav-bar"
+const navBarElements = document.querySelectorAll('.nav-bar');
+
+// Select the element with the id "dog-picture" and save it to a variable
+const dogPictureElement = document.getElementById('dog-picture');
---
+++ Week 4/4.1.2-NodeRelationships.js
@@ -0,0 +1,14 @@
+// Select the root node (the <html> element)
+const rootNode = document.documentElement;
+
+// Select the last child of the root node (the <h2> element)
+const lastChildOfRoot = rootNode.lastElementChild;
+
+// Select all the children of the body element
+const bodyChildren = document.body.children;
+
+// Select the next sibling of the h1 node (the <ul> element)
+const h1NextSibling = document.querySelector('h1').nextElementSibling;
+
+// Select the parent element of the h1 node (the <body> element)
+const h1ParentElement = document.querySelector('h1').parentElement;
